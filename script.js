3f894435a2cf1c54f1b5ea9a1e60a37cc8defeea
VitaliyKorzhevyi <malkalmmerlin@gmail.com>
2025-01-02 20:47:20 +0200

    1_step_js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const goli = "killoi";
-console.log(goli);
+const numberOfFilms = +prompt('Сколько вы уже посмотрели фильмов?', '');
+
+const personalMovieDB = {
+    count:"",
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его', '');
+
+
+personalMovieDB.movies[a] = b;    
+personalMovieDB.movies[c] = d;  
+console.log(personalMovieDB);
